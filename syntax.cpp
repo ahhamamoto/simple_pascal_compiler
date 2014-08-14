2f94312c75e46927b1c937b242ae365690861bc2
maxiiminu <maxiiminucelli@gmail.com>
2014-08-14 19:10:14 -0300

    Sintatico Pronto

--- syntax.cpp
+++ syntax.cpp
@@ -18,7 +18,7 @@
 #define DO 13
 #define OR 14
 #define DIV 15
-#define END 16
+#define AND 16
 #define NOT 17
 #define PONTOVIRGULA 18
 #define PONTO 19
@@ -47,7 +47,18 @@ int current_token;
 fstream input;
 fstream output;
 
-string PrintToken[] = {};
+void PROGRAMA();
+void BLOCO();
+void PARAMETROS_FORMAIS();
+void SUB_PARAMETROS_FORMAIS();
+void COMANDO();
+void VARIAVEL();
+void COMANDO_SEM_ROTULO();
+void EXPRESSAO();
+void EXPRESSAO_SIMPLES();
+void TERMO();
+void FATOR();
+
 
 int getToken() {
     if (!input.eof()) {
@@ -56,23 +67,321 @@ int getToken() {
     } else return 0;
 }
 
-bool eat(int token) {
-    cout << "Consumindo: [" << PrintToken[current_token] << "] (" << current_token << ")" << endl;
+void eat(int token) {
+    string PrintToken[] = { "erro","PROGRAM","LABEL","TYPE","VAR","PROCEDURE","FUNCTION","BEGIN","END",
+                            "IF","THEN","ELSE","WHILE","DO","OR","DIV","AND","NOT","PONTOVIRGULA",
+                            "PONTO","VIRGULA","DOISPONTOS","APARENTESES","FPARENTESES","IGUAL",
+                            "MENOR","MAIOR","MAIS","MENOS","ASTERISCO","ACOLCHETES","FCOLCHETES",
+                            "DOISPONTOSIGUAL","MENORMAIOR","MENORIGUAL","MAIORIGUAL","IDENTIFICADOR","NUMERO",};
+
     if (token == current_token) {
+        cout << "Consumindo: [" << PrintToken[current_token] << "] (" << current_token << ")" << endl;
         current_token = getToken();
-        return true;
     } else {
         cout << "Erro: Consumindo=>[" << PrintToken[current_token] << "]  Esperado=>[" << PrintToken[token] << "] \n";
-        getToken();
-        return false;
+        current_token = getToken();
+    }
+}
+
+void PROGRAMA() {
+    current_token = getToken();
+    eat(PROGRAM);
+    eat(IDENTIFICADOR);
+    eat(PONTOVIRGULA);
+    BLOCO();
+    eat(PONTO);
+}
+
+void BLOCO() {
+    if(current_token == LABEL) {
+        eat(LABEL);
+        eat(NUMERO);
+        while(current_token == VIRGULA) {
+            eat(VIRGULA);
+            eat(NUMERO);
+        }
+        eat(PONTOVIRGULA);
+        BLOCO();
+
+    }else if(current_token == TYPE) {
+        eat(TYPE);
+        while(current_token == IDENTIFICADOR) {
+            eat(IDENTIFICADOR);
+            eat(IGUAL);
+            eat(IDENTIFICADOR);
+            eat(PONTOVIRGULA);
+        }
+        BLOCO();
+
+    }else if(current_token == VAR) {
+        eat(VAR);
+        while(current_token == IDENTIFICADOR) {
+            eat(IDENTIFICADOR);
+            while(current_token == VIRGULA) {
+                eat(VIRGULA);
+                eat(IDENTIFICADOR);
+            }
+            eat(DOISPONTOS);
+            eat(IDENTIFICADOR);
+            eat(PONTOVIRGULA);
+        }
+        BLOCO();
+
+    }else if(current_token == PROCEDURE) {
+        eat(PROCEDURE);
+        eat(IDENTIFICADOR);
+        PARAMETROS_FORMAIS();
+        eat(PONTOVIRGULA);
+        BLOCO();
+        eat(PONTOVIRGULA);
+        BLOCO();
+
+    }else if(current_token == FUNCTION) {
+        eat(FUNCTION);
+        eat(IDENTIFICADOR);
+        PARAMETROS_FORMAIS();
+        eat(DOISPONTOS);
+        eat(IDENTIFICADOR);
+        eat(PONTOVIRGULA);
+        BLOCO();
+        eat(PONTOVIRGULA);
+        BLOCO();
+
+    }else if(current_token == BEGIN) {
+        eat(BEGIN);
+        COMANDO();
+        while(current_token == PONTOVIRGULA) {
+            eat(PONTOVIRGULA);
+            COMANDO();
+        }
+        eat(END);
+    }
+}
+
+void PARAMETROS_FORMAIS() {
+    eat(APARENTESES);
+    SUB_PARAMETROS_FORMAIS();
+    while(current_token == PONTOVIRGULA) {
+        eat(PONTOVIRGULA);
+        SUB_PARAMETROS_FORMAIS();
+    }
+    eat(FPARENTESES);
+}
+
+void SUB_PARAMETROS_FORMAIS() {
+    if(current_token == IDENTIFICADOR) {
+        eat(IDENTIFICADOR);
+        while(current_token == PONTOVIRGULA) {
+            eat(PONTOVIRGULA);
+            eat(IDENTIFICADOR);
+        }
+        eat(DOISPONTOS);
+        eat(IDENTIFICADOR);
+
+    }else if(current_token == VAR) {
+        eat(VAR);
+        eat(IDENTIFICADOR);
+        while(current_token == PONTOVIRGULA) {
+            eat(PONTOVIRGULA);
+            eat(IDENTIFICADOR);
+        }
+        eat(DOISPONTOS);
+        eat(IDENTIFICADOR);
+
+    }else if (current_token == FUNCTION) {
+        eat(FUNCTION);
+        eat(IDENTIFICADOR);
+        PARAMETROS_FORMAIS();
+        eat(DOISPONTOS);
+        eat(IDENTIFICADOR);
+
+    }else if (current_token == PROCEDURE) {
+        eat(PROCEDURE);
+        eat(IDENTIFICADOR);
+        PARAMETROS_FORMAIS();
+
+    }
+}
+
+void COMANDO() {
+    if(current_token == NUMERO) {
+        eat(NUMERO);
+        eat(DOISPONTOS);
+    }
+    COMANDO_SEM_ROTULO();
+}
+
+void VARIAVEL() {
+    eat(IDENTIFICADOR);
+    while(current_token == ACOLCHETES) {
+        eat(ACOLCHETES);
+        EXPRESSAO();
+        while(current_token == VIRGULA) {
+            EXPRESSAO();
+        }
+        eat(FCOLCHETES);
+    }
+}
+
+void COMANDO_SEM_ROTULO() {
+    if(current_token == IDENTIFICADOR) {
+        eat(IDENTIFICADOR);
+        if(current_token == DOISPONTOSIGUAL) {
+            eat(DOISPONTOSIGUAL);
+            EXPRESSAO();
+
+        }else if(current_token == APARENTESES) {
+            eat(APARENTESES);
+            if(current_token == FPARENTESES) {
+                eat(FPARENTESES);                
+            }else {
+                EXPRESSAO();
+                while(current_token == VIRGULA) {
+                    eat(VIRGULA);
+                    EXPRESSAO();
+                }
+            }
+
+        }else if(current_token == ACOLCHETES) {
+            while(current_token == ACOLCHETES) {
+                eat(ACOLCHETES);
+                EXPRESSAO();
+                while(current_token == VIRGULA) {
+                    eat(VIRGULA);
+                    EXPRESSAO();
+                }
+                eat(FCOLCHETES);
+            }
+        }
+
+    }else if(current_token == BEGIN) {
+        eat(BEGIN);
+        COMANDO_SEM_ROTULO();
+        while(current_token == PONTOVIRGULA) {
+            eat(PONTOVIRGULA);
+            COMANDO_SEM_ROTULO();
+        }
+        eat(END);
+
+    }else if(current_token == IF) {
+        eat(IF);
+        EXPRESSAO();
+        eat(THEN);
+        COMANDO_SEM_ROTULO();
+        if(current_token == ELSE) {
+            eat(ELSE);
+            COMANDO_SEM_ROTULO();
+        }
+
+    }else if(current_token == WHILE) {
+        eat(WHILE);
+        EXPRESSAO();
+        eat(DO);
+        COMANDO_SEM_ROTULO();
+    }
+}
+
+void EXPRESSAO() {
+    EXPRESSAO_SIMPLES();
+    if(current_token == IGUAL) {
+        eat(IGUAL);
+        EXPRESSAO_SIMPLES();
+    }else if(current_token == MENORMAIOR) {
+        eat(MENORMAIOR);
+        EXPRESSAO_SIMPLES();
+    }else if(current_token == MENOR) {
+        eat(MENOR);
+        EXPRESSAO_SIMPLES();
+    }else if(current_token == MENORIGUAL) {
+        eat(MENORIGUAL);
+        EXPRESSAO_SIMPLES();
+    }else if(current_token == MAIORIGUAL) {
+        eat(MAIORIGUAL);
+        EXPRESSAO_SIMPLES();
+    }else if(current_token == MAIOR) {
+        eat(MAIOR);
+        EXPRESSAO_SIMPLES();
+    }
+}
+
+void EXPRESSAO_SIMPLES() {
+    if(current_token == MAIS) {
+        eat(MAIS);
+    }else if(current_token == MENOS) {
+        eat(MENOS);
+    }
+    TERMO();
+    while(current_token == MAIS || current_token == MENOS || current_token == OR) {
+        if(current_token == MAIS) {
+            eat(MAIS);
+        }else if(current_token == MENOS) {
+            eat(MENOS);
+        }else if(current_token == OR){
+            eat(OR);
+        }
+        TERMO();
+    }
+}
+
+void TERMO() {
+    FATOR();
+    while(current_token == ASTERISCO || current_token == DIV || current_token == AND) {
+        if(current_token == ASTERISCO) {
+            eat(ASTERISCO);
+        }else if(current_token == DIV) {
+            eat(DIV);
+        }else if(current_token == AND){
+            eat(AND);
+        }
+        FATOR();
+    }
+}
+
+void FATOR() {
+    if(current_token == IDENTIFICADOR) {
+        eat(IDENTIFICADOR);
+        if(current_token == APARENTESES) {
+            eat(APARENTESES);
+            if(current_token == FPARENTESES) {
+                eat(FPARENTESES);                
+            }else {
+                EXPRESSAO();
+                while(current_token == VIRGULA) {
+                    eat(VIRGULA);
+                    EXPRESSAO();
+                }
+            }
+
+        }else if(current_token == ACOLCHETES) {
+            while(current_token == ACOLCHETES) {
+                eat(ACOLCHETES);
+                EXPRESSAO();
+                while(current_token == VIRGULA) {
+                    eat(VIRGULA);
+                    EXPRESSAO();
+                }
+                eat(FCOLCHETES);
+            }
+        }
+
+    }else if(current_token == APARENTESES) {
+        eat(APARENTESES);
+        EXPRESSAO();
+        eat(FPARENTESES);
+    }else if(current_token == NOT) {
+        eat(NOT);
+        FATOR();
+    }else if(current_token == NUMERO) {
+        eat(NUMERO);
     }
 }
 
 int main(int argc, char **argv) {
 	if (argc != 3) {
-        cerr << "Uso: lexico [Nome do Arquivo de Entrada] [Nome do Arquivo de Saida]" << endl;
+        cerr << "Uso: Sintatico [Nome do Arquivo de Entrada] [Nome do Arquivo de Saida]" << endl;
         return(1);
     }
+
     input.open(argv[1], fstream::in);
     output.open(argv[2], fstream::out);
 
@@ -85,6 +394,8 @@ int main(int argc, char **argv) {
         cout << "file not open (saida)";
         return 0;
     }
+
+    PROGRAMA();
 
     return 1;	
 }
